592030b9ba84a9d88a88c5fcc76dcb889a44ac3b
areema578 <30280730+areema578@users.noreply.github.com>
2018-09-18 14:29:50 -0700

    Add files via upload

---
+++ TesterIPI.java
@@ -0,0 +1,32 @@
+package projectIPI;
+
+import java.util.Scanner;
+/**
+ *
+ * @author howar
+ */
+public class TesterIPI {
+    
+    public static void main(String[] args) {
+        int selection;
+        
+        do {
+        System.out.println("Make a selection from our Dessert Menu\n1.Sour Patch"
+                + " Kids- 99 cents a lb\n2.Choclate Chip Cookie- 399 per dozen\n"
+                + "3.Ice cream- 1.00 per scoop, 1.00 per topping");
+        Scanner in = new Scanner(System.in);
+        selection = in.nextInt();
+        
+        if ( selection == 1) {
+            
+        } else if (selection == 2) {
+            
+        } else if (selection == 3) {
+            
+        }
+        }while selection = 4
+
+
+
+    }
+}
